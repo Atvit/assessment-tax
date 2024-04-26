1ddbdd1731bc535eb3366411e5530d6326d980e8
Atthawit Boonrassamee <attawitt@live.com>
2024-04-27 02:35:43 +0700

    feat: add tax calculation handler

---
+++ internals/tax/handler.go
@@ -0,0 +1,70 @@
+package tax
+
+import (
+	"github.com/Atvit/assessment-tax/utils"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+type Allowance struct {
+	AllowanceType string  `json:"allowanceType" validate:"omitempty,oneof=donation k-receipt"`
+	Amount        float64 `json:"amount" validate:"omitempty,gt=0"`
+}
+
+type Request struct {
+	TotalIncome float64     `json:"totalIncome" validate:"required,gte=0"`
+	Wht         float64     `json:"wht" validate:"omitempty,gte=0,ltefield=TotalIncome"`
+	Allowances  []Allowance `json:"allowances" validate:"dive"`
+}
+
+type Response struct {
+	Tax float64 `json:"tax"`
+}
+
+type Handler interface {
+	CalculateTax(c echo.Context) error
+}
+
+type handler struct {
+	logger   *zap.Logger
+	validate *validator.Validate
+}
+
+func NewHandler(logger *zap.Logger, validate *validator.Validate) Handler {
+	return handler{
+		logger:   logger,
+		validate: validate,
+	}
+}
+
+func (h handler) CalculateTax(c echo.Context) error {
+	var req Request
+
+	if err := c.Bind(&req); err != nil {
+		h.logger.Error("binding request failed", zap.Error(err))
+		return c.JSON(http.StatusBadRequest, utils.ErrResponse{
+			Error: err.Error(),
+		})
+	}
+
+	if err := h.validate.Struct(req); err != nil {
+		h.logger.Error("validate request body failed", zap.Error(err))
+		return c.JSON(http.StatusBadRequest, utils.ErrResponse{
+			Error: utils.GetValidateErrMsg(err),
+		})
+	}
+
+	result, err := calculate(req.TotalIncome)
+	if err != nil {
+		h.logger.Error("tax calculation failed", zap.Error(err))
+		return c.JSON(http.StatusBadRequest, utils.ErrResponse{
+			Error: err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, Response{
+		Tax: result,
+	})
+}
