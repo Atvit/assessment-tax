6bbe42b98393aa78b8a981f65914327edafd1df4
Atthawit Boonrassamee <attawitt@live.com>
2024-04-27 02:40:44 +0700

    fix: receive cfg as pointer

--- middleware/auth.go
+++ middleware/auth.go
@@ -4,6 +4,6 @@ import (
 	"github.com/Atvit/assessment-tax/config"
 )
 
-func Authenticate(u, p string, cfg config.Configuration) (bool, error) {
+func Authenticate(u, p string, cfg *config.Configuration) (bool, error) {
 	return u == cfg.AdminUsername && p == cfg.AdminPassword, nil
 }
