f377cb737d2a799ca76b4b4b9bc03d664c51d44e
Atthawit Boonrassamee <attawitt@live.com>
2024-04-27 19:02:15 +0700

    test: add test case to cover calculate allowance and validate allowance type

--- internals/tax/tax_test.go
+++ internals/tax/tax_test.go
@@ -7,11 +7,12 @@ import (
 
 func TestCalculateTax(t *testing.T) {
 	tests := []struct {
-		name    string
-		income  float64
-		wht     float64
-		wantTax float64
-		wantErr error
+		name       string
+		income     float64
+		wht        float64
+		wantTax    float64
+		wantErr    error
+		allowances []TaxAllowance
 	}{
 		{
 			name:    "Negative Income",
@@ -106,13 +107,57 @@ func TestCalculateTax(t *testing.T) {
 			wantTax: 0,
 			wantErr: errs.ErrWhtMustLowerThanOrEqualIncome,
 		},
+		{
+			name:    "Invalid Allowance Type",
+			income:  500000,
+			wht:     0,
+			wantTax: 0,
+			allowances: []TaxAllowance{
+				{AllowanceType: personal, Amount: 40000},
+				{AllowanceType: "invalid", Amount: 50000},
+			},
+			wantErr: errs.ErrIncorrectAllowanceType,
+		},
+		{
+			name:       "Allowance Amount Equal Zero",
+			income:     500000,
+			wht:        0,
+			wantTax:    0,
+			allowances: []TaxAllowance{{AllowanceType: personal, Amount: 0}},
+			wantErr:    errs.ErrValueMustBePositive,
+		},
+		{
+			name:       "Allowance Amount Less Than Zero",
+			income:     500000,
+			wht:        0,
+			allowances: []TaxAllowance{{AllowanceType: personal, Amount: -1000}},
+			wantTax:    0,
+			wantErr:    errs.ErrValueMustBePositive,
+		},
+		{
+			name:       "Empty Allowance Slice",
+			income:     500000,
+			wht:        0,
+			allowances: []TaxAllowance{},
+			wantTax:    29000,
+			wantErr:    nil,
+		},
+		{
+			name:       "With Donation Allowance",
+			income:     500000,
+			wht:        0,
+			allowances: []TaxAllowance{{AllowanceType: donation, Amount: 200000}},
+			wantTax:    19000,
+			wantErr:    nil,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			gotTax, gotErr := Calculate(&Tax{
-				Income: tt.income,
-				Wht:    tt.wht,
+				Income:     tt.income,
+				Wht:        tt.wht,
+				Allowances: tt.allowances,
 			})
 			if gotErr != tt.wantErr {
 				t.Errorf("calculateTax error = %v, wantErr %v", gotErr, tt.wantErr)
