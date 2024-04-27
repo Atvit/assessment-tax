51988614e106eff91a987fddcd2cc2b68f713e50
Atthawit Boonrassamee <attawitt@live.com>
2024-04-28 03:36:40 +0700

    refactor: store err message to variable

--- utils/get_validate_err_msg.go
+++ utils/get_validate_err_msg.go
@@ -8,6 +8,15 @@ import (
 
 const UnknownErrMsg = "unknown error"
 
+const (
+	required = "field %s is required"
+	oneof    = "the value of %s must be one of %s"
+	gt       = "the value of %s must be greater than %s"
+	gte      = "the value of %s must be greater than or equal %s"
+	lte      = "the value of %s must be less than or equal %s"
+	ltefield = "the value of %s value must be lower than or equal value of field %s"
+)
+
 type FieldErr struct {
 	Field   string `json:"field"`
 	Message string `json:"message"`
@@ -16,15 +25,17 @@ type FieldErr struct {
 func getErrMsg(fe validator.FieldError) string {
 	switch fe.Tag() {
 	case "required":
-		return fmt.Sprintf("field %s is required", fe.Field())
+		return fmt.Sprintf(required, fe.Field())
 	case "oneof":
-		return fmt.Sprintf("the value of %s must be one of %s", fe.Field(), fe.Param())
+		return fmt.Sprintf(oneof, fe.Field(), fe.Param())
 	case "gt":
-		return fmt.Sprintf("the value of %s must be greater than %s", fe.Field(), fe.Param())
+		return fmt.Sprintf(gt, fe.Field(), fe.Param())
 	case "gte":
-		return fmt.Sprintf("the value of %s must be greater than or equal %s", fe.Field(), fe.Param())
+		return fmt.Sprintf(gte, fe.Field(), fe.Param())
+	case "lte":
+		return fmt.Sprintf(lte, fe.Field(), fe.Param())
 	case "ltefield":
-		return fmt.Sprintf("the value of %s value must be lower than or equal value of field %s", fe.Field(), fe.Param())
+		return fmt.Sprintf(ltefield, fe.Field(), fe.Param())
 	}
 
 	return UnknownErrMsg
