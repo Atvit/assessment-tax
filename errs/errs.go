ed23d857a8a665112bfa18c6129f6ce05e5860e5
Atthawit Boonrassamee <attawitt@live.com>
2024-04-28 12:08:17 +0700

    feat: implement upload csv tax functionalities

--- errs/errs.go
+++ errs/errs.go
@@ -6,4 +6,5 @@ var (
 	ErrValueMustBePositive           = errors.New("value must be positive")
 	ErrWhtMustLowerThanOrEqualIncome = errors.New("with holding tax must be lower than or equal to income")
 	ErrIncorrectAllowanceType        = errors.New("incorrect allowance type")
+	ErrEmptyCsv                      = errors.New("empty csv file given")
 )
